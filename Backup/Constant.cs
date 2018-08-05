8d6d4e5874a7d8edb857a63aa41b203d8cb975cc
VEYTIAONE <veytia139@hotmail.com>
2018-08-05 16:23:14 -0500

    Revert "Add .gitignore and .gitattributes."
    This reverts commit d28c728704f8bd6ef306a3f783524110bfccc82e.

---
+++ Backup/Constant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadTrxFile
+{
+    public static class Constant
+    {
+        #region Column
+        public static string PROCESSEDFILENAME = "ProcessedFileName";
+        public static string TESTID = "TestID";
+        public static string TESTNAME = "TestName";
+        public static string TESTOUTCOME = "TestOutcome";
+        public static string ERRORMESSAGE = "ErrorMessage";
+        #endregion
+    }
+}
